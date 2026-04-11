478b089d75020aae35bbd81879652665710ccba3
firas-aghbar <faghbar@itgsoftware.com>
2026-04-11 18:41:23 +0300

    embed all chunks

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
 import os
-import re
-import time
 import logging
-import ast
 from sentence_transformers import SentenceTransformer
 import faiss
 import numpy as np
 
 from chunking.chunk_code import chunk_codebase
 from chunking.chunk_logs import chunk_logs
+
+def embed_chunks(chunks, embed_model):
+    texts = [c["content"] for c in chunks]
+
+    vectors = embed_model.encode(texts, show_progress_bar=True)
+    return np.array(vectors).astype("float32")
+
+def build_faiss_index(vectors):
+    dim = vectors.shape[1]
+    index = faiss.IndexFlatL2(dim)
+    index.add(vectors)
+    return index
+
+def search(query, index, chunks, top_k=50):
+    # 1. embed query
+    query_vec = embed_model.encode([query]).astype("float32")
+
+    # 2. search FAISS
+    distances, indices = index.search(query_vec, top_k)
+
+    # 3. map results back to chunks
+    results = [chunks[i] for i in indices[0]]
+
+    return results
 
 if __name__ == "__main__":
     path = "log files/almatarhotels_run_prod_79.log"
@@ -22,10 +43,13 @@ if __name__ == "__main__":
     log_chunks = chunk_logs(path, size_mb)
 
     code_chunks = chunk_codebase(code_repo_dir)
-    code_end_time = time.time()
-    logging.info(f"took {code_end_time - code_start_time} to chunk codebase")
 
+    chunks = code_chunks + log_chunks
 
+    embed_model = SentenceTransformer("mixedbread-ai/mxbai-embed-large-v1")
 
+    vectors = embed_chunks(chunks, embed_model)
 
+    faiss_indexes = build_faiss_index(vectors)
 
+    results = search("why are some requests failing?", faiss_indexes, chunks)
